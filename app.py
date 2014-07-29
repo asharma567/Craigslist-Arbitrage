ca0e34ddb1cdcb2ffa6a257ca6ed151e9956978d
asharma567 <asharma567@gmail.com>
2014-07-29 10:43:27 -0700

    refactoring

--- app.py
+++ app.py
@@ -4,105 +4,83 @@ import pandas as pd
 import sqlalchemy as sql
 from scraper.scraper import get_training_data
 from datetime import timedelta
-from pricer.pricing import grab_data_for_analysis, modeled_indices, make_pricing_panel
+from pricer.pricing import grab_data_for_analysis, modeled_indices
+from pricer.pricing_panel import make_pricing_panel
 from pricer.utils_pricer import preprocess_from_df, clean_dead_links
+from utils import db_conn
+import cPickle
 
 app = Flask(__name__)
 
-#Ask gio where to put this
-headingopen = '<h1 id="firstHeading" class="firstHeading">'
-headingclose = '</h1>'
-
-bodyopen = '<div id="bodyContent"><p>'
-bodyclose = '</p></div>'
-
-linkopen = '<a href='
-linkmid = '>'
-linkclose = '</a>'
-
-footopen = '<p>'
-footend = '</p>'
+#EDIT put this in the row function
 
 
-def display_panel(df, indices, top_n = 10):
+def row_to_html(df_row):
+    '''
+    Row of DataFrame -> Google Api Infowindow post
+    '''
+    headingopen = '<h1 id="firstHeading" class="firstHeading">'
+    headingclose = '</h1>'
+
+    bodyopen = '<div id="bodyContent"><p>'
+    bodyclose = '</p></div>'
+
+    linkopen = '<a href='
+    linkmid = '>'
+    linkclose = '</a>'
+
+    footopen = '<p>'
+    footend = '</p>'
+
+    # All properties of an Infowindow Google Maps Api
+    heading = headingopen + linkopen + str(df_row['url_to_post']) + \
+                linkmid + str(df_row['heading']) + linkclose + headingclose 
+    
+    body = bodyopen + str(df_row['body']) + bodyclose 
+    
+    footer = footopen + str(df_row['px']) + ' posted - ' + str(df_row['ebay_price']) + \
+             ' eBay  = ' + str(df_row['spread']) +' profit'+ footend
+    
+    Latitude = float(df_row['LatLng'][0]), 
+    Longitude = float(df_row['LatLng'][1])
+    infowindow = heading + body + footer
+    return [infowindow, Latitude[0], Longitude]
+
+
+def display_panel(df, indices, top_n= 10):
     columns = ['heading', 'year', 'px','url_to_post','LatLng','body']
     new_df      = df.iloc[indices][columns][:top_n]
 
-    return make_pricing_panel(new_df).sort('spread')[::-1]
+    return make_pricing_panel(new_df, PXS).sort('spread')[::-1]
 
-
-
-@app.route('/dashboard')
+@app.route('/')
 def dashboard():
     metro = 'USA-NYM'
     df = grab_data_for_analysis(metro, engine)
-    X, y = preprocess_from_df(df['year'],df['px'])
-    #NOTE - Arb
+    X, y = preprocess_from_df(df['year'], df['px'])
     # show_this_df = clean_dead_links(display_panel(df, modeled_indices(X, y, 'RF'), 20))
     
-    show_this_df = display_panel(df, modeled_indices(X, y, 'RF'), 20)
+    show_this_df = display_panel(df, modeled_indices(X, y, df), 20)
+    arb_only_show_this_df = show_this_df[show_this_df['spread'] > 0]
+
     
     #gets list with highest profit margins vs eBay
-    locs = [[headingopen + linkopen + str(row['url_to_post']) + linkmid +  \
-                str(row['heading']) + linkclose + headingclose + \
-                bodyopen + str(row['body']) + bodyclose + \
-                footopen + str(row['px']) + ' posted - ' + str(row['ebay_price']) + ' eBay  = ' + str(row['spread']) +' profit'+ footend, \
-                float(row['LatLng'][0]), float(row['LatLng'][1])] \
-                for index, row in show_this_df.iterrows()]
+    #EDIT
+    locs = [row_to_html(row) for index, row in clean_dead_links(arb_only_show_this_df).iterrows()]
     
-    
-    html_page = render_template('map_page.html.jinja', 
-            df= show_this_df.to_html(),
-            l= locs,
-            links= show_this_df)
+    html_page = render_template('map_page.html.jinja2', 
+                                    df=arb_only_show_this_df.to_html(),
+                                    l=locs,
+                                    links=arb_only_show_this_df)
 
     return html_page
 
-# @app.route('/dashboard?city = NYC')
-# def dashboard():
-#     return render_template('some.html.jinja', predictions=grab_data())
-
 if __name__ == '__main__':
-    # Setup database connection
-    engine = sql.create_engine('postgresql://Ajay:@localhost/arbitrage')
-    if engine.connect(): 
-        print 'Connected to SQL DB'
-
-    flask_app_port = 5233
-
-    app.run(host='0.0.0.0', port=flask_app_port, debug=True)
+    engine = db_conn()
+    PXS = cPickle.load(open('scraper/ebay_data.pkl', 'r'))
+    app.run(host='0.0.0.0', port=80, debug=True)
 
 
-'''
-Random jinja notes
-{% for index, row in df.iterrows() %}
-<tr>
-<td>{{ row['year'] }}</td>
-<td>{{ row['px'] }}</td>
-</tr></br>
-{% endfor %}
-'''
-# <html>
-# <body>
-
-# {% block content %}
-# <h1>Hi!</h1>
-# {{df | safe}}
-# {% endblock %}
-
-# {% for index, row in links.iterrows() %}
-# <tr>
-# {{ row['year'] }}
-# {{ row['ebay_price'] }}
-# {{ row['px'] }}
-# {{ row['spread'] }}
-# <a href={{ row['url_to_post'] }}> {{ row['heading'] }} </a>
-
-# </tr></br>
-# {% endfor %}
-
-# </body>
-# </html>
 
 
 
